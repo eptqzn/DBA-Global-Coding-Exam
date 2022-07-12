5d5fbcfe2c0867a0e95f23c6dc39b96d6e985928
Enrich Quiazon <104322730+eptqzn@users.noreply.github.com>
2022-07-12 23:08:08 +0800

    Update examone.java

--- examone.java
+++ examone.java
@@ -1,9 +1,7 @@
 public class examone {  
-    public static void main(String[] args) {  
-            //Initialize array  
+    public static void main(String[] args) {   
             int [] arr = new int [] {5, 1, 4, 6, 7, 3, 5, 7, 3};  
             System.out.println("Duplicate elements in given array: ");  
-            //Searches for duplicate element  
             for(int i = 0; i < arr.length; i++) {  
                 for(int j = i + 1; j < arr.length; j++) {  
                     if(arr[i] == arr[j])  
